46c7b7d963d01956d8ce530467489aa815892a67
Haley Pearse <10296672+haleypearse@users.noreply.github.com>
2023-02-22 06:33:46 -0800

    initial

---
+++ Models.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MicroGrad
+{
+    public static class Global
+    {
+        public static int ValueCount { get; set; } = 1;
+    }
+
+    public class Value
+    {
+        public Value(double data)
+        {
+            Data = data;
+        }
+
+        public double Data { get; set; }
+        public int Index { get; set; } = Global.ValueCount++;
+        public List<Value> Children { get; set; } = new List<Value>();
+        public List<Value> Prev { get => Children.Distinct().ToList(); }
+        public char Op { get; set; }    
+
+        public static Value operator -(Value a) => new Value(-a.Data);
+        public static Value operator +(Value a, Value b) => new Value(a.Data + b.Data) { Children = new[] { a, b }.ToList(), Op = '+' };
+        public static Value operator *(Value a, Value b) => new Value(a.Data * b.Data) { Children = new[] { a, b }.ToList(), Op = '*' };
+
+    }
+
+
+    // Operator Overloading Example
+    public readonly struct Fraction
+    {
+        private readonly int num;
+        private readonly int den;
+
+        public Fraction(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new ArgumentException("Denominator cannot be zero.", nameof(denominator));
+            }
+            num = numerator;
+            den = denominator;
+        }
+
+        public static Fraction operator +(Fraction a) => a;
+        public static Fraction operator -(Fraction a) => new Fraction(-a.num, a.den);
+
+        public static Fraction operator +(Fraction a, Fraction b)
+            => new Fraction(a.num * b.den + b.num * a.den, a.den * b.den);
+
+        public static Fraction operator -(Fraction a, Fraction b)
+            => a + (-b);
+
+        public static Fraction operator *(Fraction a, Fraction b)
+            => new Fraction(a.num * b.num, a.den * b.den);
+
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            if (b.num == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return new Fraction(a.num * b.den, a.den * b.num);
+        }
+
+        public override string ToString() => $"{num} / {den}";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using MermaidNetHtmlBuilder;
+using System.IO;
+using System.Security.Principal;
+using System.Xml;
+using HtmlAgilityPack;
+using HtmlAgilityPack.CssSelectors.NetCore;
+using System.IO.Compression;
+using FluentMermaid.Enums;
+using FluentMermaid.Flowchart;
+using FluentMermaid.Flowchart.Enum;
+using System.Text.RegularExpressions;
+using FluentMermaid;
+
+namespace MicroGrad
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            NNFun();
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static void NNFun()
+        {
+            var a = 3.0.Value();
+            var b = 4.0.Value();
+            var c = a * b;
+            var d = 5.0.Value();
+            var e = c + d;
+
+
+            CreateDiagram(e);
+
+
+        }
+
+        private static void CreateDiagram(Value root)
+        {
+            var nodes = new Dictionary<Value, INode>(); // new List<INode>();
+            var chart = FlowChart.Create(Orientation.LeftToRight);
+
+            Build(root);
+            void Build(Value node)
+            {
+                INode iNode;
+                if (!nodes.ContainsKey(node))
+                {
+                    iNode = chart.TextNode($"{node.Index}: {node.Data.ToString()}", Shape.RoundEdges);
+                    nodes.Add(node, iNode);
+                }
+                else
+                    iNode = nodes[node];
+
+                var opNode = chart.TextNode(node.Op.ToString(), Shape.Circle);
+                chart.Link(opNode, nodes[node], Link.Arrow, "");
+                foreach (var child in node.Children)
+                {
+                    Build(child);
+                    chart.Link(nodes[child], opNode, Link.Arrow, "");
+
+
+                }
+            }
+
+
+
+            var e = new List<INode>();
+
+            string mermaidSyntax = chart.Render();// Regex.Unescape(chart.Render());
+            ; 
+
+            //var outputHtml = new HtmlDocument();
+            //outputHtml.LoadHtml("<html><body></body></html>");
+            //var body = outputHtml.QuerySelector("body");
+
+            //var merm = new MermaidHtmlBuilder();
+
+            //var builder = new MermaidHtmlBuilder();
+            //var descriptions = new Dictionary<string, string>();
+            //var outDir = Path.GetDirectoryName(@"C:\Users\Haley\OneDrive - StandardFusion\SF\");
+            //var outFileName = "Mermaid Test.zip"
+            //;
+            ////builder.SaveZip(Path.GetDirectoryName(inputXml) + outFileName, diagram, descriptions);
+            //var zipArray = builder.BuildZip(diagram, descriptions);
+            //var zipStream = new MemoryStream();
+            //zipStream.Write(zipArray, 0, zipArray.Length);
+            //var zip = new ZipFile(zipStream);
+
+            //ZipInputStream inStream = new ZipInputStream(zipStream);
+
+
+            //foreach (ZipEntry ze in zip)
+            //    if (ze.Name == "index.html")
+            //    {
+            //        string text = new StreamReader(zip.GetInputStream(ze)).ReadToEnd();
+
+            //        var html = new HtmlDocument();
+            //        html.LoadHtml(text);
+            //        var incomingBody = HtmlNode.CreateNode(html.QuerySelector("body").OuterHtml);
+            //        scripts = incomingBody.QuerySelectorAll("script").ToList();
+
+            //        var workflowHtmlSection = HtmlNode.CreateNode("<div></div>");
+            //        workflowHtmlSection.AppendChild(HtmlNode.CreateNode($"<h2>{workflowName}</h2>"));
+            //        workflowHtmlSection.AppendChild(incomingBody.QuerySelector(".mermaid"));
+            //        //if (body.QuerySelector("script") == null)
+            //        //    body.AppendChildren(scripts);
+            //        //else;
+
+            //        body.AppendChild(workflowHtmlSection);
+            //    }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+
+    public static class ExtensionMethods
+    {
+        public static Value Value(this double val) => new Value(val);
+    }
+}
